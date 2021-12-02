e664e77847407dc31741c811aeb0d721812bec30
Hosea Kandie <zeddarn@gmail.com>
2021-12-02 17:21:13 -0600

    Day 1 Part Two

---
+++ day01/path-02/code.py
@@ -0,0 +1,22 @@
+import os
+
+filename = os.path.dirname(__file__) + "\\input"
+
+with open(filename) as file:
+    current = k = last = i = 0
+    currentSum = []
+    for line in file:
+        current = int(line.rstrip())
+        currentSum.append(current)
+
+        if len(currentSum) > 3:
+            current = sum(currentSum[0:3])
+            last = sum(currentSum[1:4])
+            currentSum.pop(0)
+
+            if current < last:
+                k += 1
+
+        last = current
+
+    print(k)
